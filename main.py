a484e11b296be7a59cfa01c47bdb799cfe2e0555
DerVogel101 <jan.irmer@outlook.de>
2023-03-25 18:10:30 +0100

    commit

--- main.py
+++ main.py
@@ -2,7 +2,36 @@
 import subprocess
 import json
 
-limit = int(input("Bitte die Maximale Zahl angeben bis zu welcher getestet werden soll: "))
+# Eingabe der max. Zahl und der Anzahl der Prozesse
+while True:
+    try:
+        limit = int(input("Bitte die maximale Zahl angeben bis zu welcher getestet werden soll: "))
+        if limit <= 0:
+            raise ValueError
+        break
+    except ValueError:
+        print("Ungültige Eingabe. Bitte geben Sie eine positive Ganzzahl ein.")
+
+while True:
+    try:
+        proz_anzahl = int(input("Bitte die Anzahl der Prozesse angeben: "))
+        if proz_anzahl > limit:
+            raise ValueError
+        break
+    except ValueError:
+        print(f"Ungültige Eingabe. Bitte geben Sie eine positive Ganzzahl ein, die kleiner oder gleich der max. Zahl: {limit} ist.")
+
+# teile limit auf die Anzahl der Prozesse auf so,
+# dass sie in einer liste sind z.b. [[0, 100], [101, 200], [201, 300]] für ein limit von 300 und 3 Prozessen
+
+prozesse = [[0, (0 + 1) * limit // proz_anzahl]]
+
+for i in range(1, proz_anzahl):
+    prozesse.append([(i * limit // proz_anzahl) + 1, (i + 1) * limit // proz_anzahl])
+print(prozesse)
+
+for proz_num in range(proz_anzahl):
+    exec(f'p{proz_num} = subprocess.Popen(["python", "calculate.py", str(prozesse[proz_num]), str(proz_num), "2"], stdout=subprocess.PIPE)')
 
 p1 = subprocess.Popen(['python', 'calculate.py', str(limit), '1', '2'], stdout=subprocess.PIPE)
 p2 = subprocess.Popen(['python', 'calculate.py', str(limit), '2', '2'], stdout=subprocess.PIPE)
