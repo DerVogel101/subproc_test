f7d78d7e654fd5f84deaad368f1549408143d1bf
DerVogel101 <jan.irmer@outlook.de>
2023-03-26 19:56:16 +0200

    divisor calculation

--- calculate.py
+++ calculate.py
@@ -2,9 +2,9 @@
 import sys
 import json
 
-def calculate(limit, start, step):
-    teiler_liste = [0] * (limit + 1)
-    for zahl_auswahl in range(start, limit + 1, step):
+def calculate(limit_start, limit_end, limit):
+    teiler_liste = [[]] * (limit + 1)
+    for zahl_auswahl in range(limit_start, limit_end + 1):
         teiler_liste[zahl_auswahl] = []
         for teiler_test in range(1, int(zahl_auswahl ** 0.5) + 1):
             if zahl_auswahl % teiler_test == 0:
@@ -13,8 +13,12 @@ def calculate(limit, start, step):
                     teiler_liste[zahl_auswahl].append(zahl_auswahl // teiler_test)
     print(json.dumps(teiler_liste))
 
+
 if __name__ == '__main__':
-    limit = int(sys.argv[1])
-    start = int(sys.argv[2])
-    step = int(sys.argv[3])
-    calculate(limit, start, step)
+    limit_bereich = str(sys.argv[1])
+    limit = int(sys.argv[2])
+    # wandle den string in eine liste um
+    limit_bereich = limit_bereich[1:-1].split(', ')
+    limit_start = int(limit_bereich[0])
+    limit_end = int(limit_bereich[1])
+    calculate(limit_start, limit_end, limit)
