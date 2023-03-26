611cf43b023abbb4111fc591b6911a9042ca8e8d
DerVogel101 <jan.irmer@outlook.de>
2023-03-26 21:33:50 +0200

    divisor calculation

--- calculate.py
+++ calculate.py
@@ -3,14 +3,17 @@ import sys
 import json
 
 def calculate(limit_start, limit_end, limit):
-    teiler_liste = [[]] * (limit + 1)
+    teiler_liste = []
+    schritt = 0
     for zahl_auswahl in range(limit_start, limit_end + 1):
-        teiler_liste[zahl_auswahl] = []
+        teiler_liste.append([])
         for teiler_test in range(1, int(zahl_auswahl ** 0.5) + 1):
+
             if zahl_auswahl % teiler_test == 0:
-                teiler_liste[zahl_auswahl].append(teiler_test)
+                teiler_liste[schritt].append(teiler_test)
                 if teiler_test != zahl_auswahl // teiler_test and zahl_auswahl != zahl_auswahl // teiler_test:
-                    teiler_liste[zahl_auswahl].append(zahl_auswahl // teiler_test)
+                    teiler_liste[schritt].append(zahl_auswahl // teiler_test)
+        schritt += 1
     print(json.dumps(teiler_liste))
 
 
