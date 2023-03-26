54a8416d70a2f0aa17a1c8b9599087c46934ab80
DerVogel101 <jan.irmer@outlook.de>
2023-03-26 21:34:34 +0200

    main process

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ while True:
         else:
             print(f"Ungültige Eingabe. Bitte geben Sie eine positive Ganzzahl ein, die kleiner oder gleich der max. Zahl: {limit} ist.")
 
+teiler_liste = []
 # teile limit auf die Anzahl der Prozesse auf so,
 # dass sie in einer liste sind z.b. [[0, 100], [101, 200], [201, 300]] für ein limit von 300 und 3 Prozessen
 
@@ -44,20 +45,11 @@ for proz_num in range(proz_anzahl):
     exec(f'p{proz_num} = subprocess.Popen(["python", "calculate.py", str(prozesse[proz_num]), str(limit)], stdout=subprocess.PIPE)')
 
 for proz_num in range(proz_anzahl):
-    exec(f'result{proz_num} = json.loads(p{proz_num}.communicate()[0])')
-    # ersetze [] mit [0]
-    exec(f'result{proz_num} = [[0] if x == [] else x for x in result{proz_num}]')
+    exec(f'teiler_erg_{proz_num} = json.loads(p{proz_num}.communicate()[0])')
+    exec(f'teiler_liste += teiler_erg_{proz_num}')
+print("teiler berechnen fertig")
 
 
-for proz_num in range(proz_anzahl):
-    exec(f'print(result{proz_num})')
-
-'''teiler_liste = [None] * (limit + 1)
-for i in range(1, limit + 1):
-    if i % 2 == 1:
-        teiler_liste[i] = result1[i]
-    else:
-        teiler_liste[i] = result2[i]
-
-print(teiler_liste)'''
+print(teiler_liste)
+print("fertig")
 
